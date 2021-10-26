bc362d289a699aff0a46b05d353158342de73567
KTUYTS <75030410+KTUYTS@users.noreply.github.com>
2021-10-26 08:04:29 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,16 +5,10 @@ import {Checkbox,Select,MenuItem} from '@material-ui/core'
 const empList = [
 
 
-  { date: "12/12/2021", sb_name: "Math", block: '2', teacher: "Nguyen Van A", year: 2019-2020 },
-  { date: "02/12/2020", sb_name: "Math", block: '3', teacher: "Nguyen Van A", year: 2020-2021 },
-  { date: "22/12/2021", sb_name: "Math", block: '4', teacher: "Nguyen Van A", year: 2021-2022 },
-  { date: "12/12/2020", sb_name: "Math", block: '5', teacher: "Nguyen Van A", year: 2020-2021 },
-  { date: "22/12/2022", sb_name: "Math", block: '5', teacher: "Nguyen Van A", year: 2021-2022 },
-  { date: "12/12/2021", sb_name: "Math", block: '2', teacher: "Nguyen Van B", year: 2019-2020 },
-  { date: "02/12/2020", sb_name: "Math", block: '3', teacher: "Nguyen Van C", year: 2020-2021 },
-  { date: "22/12/2021", sb_name: "Math", block: '4', teacher: "Nguyen Van E", year: 2021-2022 },
-  { date: "12/12/2020", sb_name: "Math", block: '5', teacher: "Nguyen Van F", year: 2020-2021 },
-  { date: "22/12/2022", sb_name: "Math", block: '5', teacher: "Nguyen Van G", year: 2021-2022 },
+  { class: "12A1", block: "1", week: "2", time: "08:00 - 09:00", monday: "Math", tuesday: "Lit", wednesday: "Chem", thursday: "Infor", friday: "edu", saturday: "none", year: "2020-2021" },
+  { class: "11A1", block: "1", week: "2", time: "08:00 - 09:00", monday: "Math", tuesday: "Lit", wednesday: "Chem", thursday: "Infor", friday: "edu", saturday: "none", year: "2019-2020" },
+  { class: "10A1", block: "1", week: "2", time: "08:00 - 09:00", monday: "Math", tuesday: "Lit", wednesday: "Chem", thursday: "Infor", friday: "edu", saturday: "none", year: "2021-2022" },
+  
 
 
 ]
@@ -24,10 +18,15 @@ function App() {
  const [filter, setFilter]=useState(true)
  const [year,setYear]=useState('all')
   const columns = [
-    { title: "Date", field: "date" },
-    { title: "Subject Name", field: "sb_name" },
+    { title: "Class", field: "class" },
     { title: "Block", field: "block" },
-    { title: "Teacher", field: 'teacher' },
+    { title: "Time", field: "time" },
+    { title: "Monday", field: "monday" },
+    { title: "Tuesday", field: "tuesday" }, 
+    { title: "Wednesday", field: 'wednesday' },
+    { title: "Thursday", field: 'thursday' },
+    { title: "Friday", field: "friday" },
+    { title: "Saturday", field: "saturday" },
     
   ]
   const handleChange=()=>{
@@ -41,7 +40,7 @@ setFilteredData(year==='all'?empList:empList.filter(dt=>dt.year===year))
   return (
     <div className="App">
       <h1 align="center"></h1>
-      <h4 align='center'>Student Time Table</h4>
+      <h4 align='center'>Time Table Full School</h4>
       
       
       <MaterialTable
@@ -70,15 +69,18 @@ setFilteredData(year==='all'?empList:empList.filter(dt=>dt.year===year))
             onChange={(e)=>setYear(e.target.value)}
           >
              <MenuItem value={"all"}><em>All</em></MenuItem>
-            <MenuItem value={2019-2020}>2019-2020</MenuItem>
-            <MenuItem value={2020-2021}>2020-2021</MenuItem>
-            <MenuItem value={2021-2022}>2021-2022</MenuItem>
+            <MenuItem value={"2019-2020"}>2019-2020</MenuItem>
+            <MenuItem value={"2020-2021"}>2020-2021</MenuItem>
+            <MenuItem value={"2021-2022"}>2021-2022</MenuItem>
           </Select>,
           tooltip:"Filter Year",
           isFreeAction:true
 
           
           },
+
+
+  
 
 
   
