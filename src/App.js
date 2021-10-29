6d6c1240eb544d60945885c9ad2f2f230faf3390
KTUYTS <75030410+KTUYTS@users.noreply.github.com>
2021-10-29 16:04:47 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,46 +2,58 @@ import React, { useState,useEffect } from 'react';
 import './App.css';
 import MaterialTable from 'material-table'
 import {Checkbox,Select,MenuItem} from '@material-ui/core'
-const empList = [
 
 
-  { date: "12/12/2021", sb_name: "Math", block: '2', teacher: "Nguyen Van A", year: 2019-2020 },
-  { date: "02/12/2020", sb_name: "Math", block: '3', teacher: "Nguyen Van A", year: 2020-2021 },
-  { date: "22/12/2021", sb_name: "Math", block: '4', teacher: "Nguyen Van A", year: 2021-2022 },
-  { date: "12/12/2020", sb_name: "Math", block: '5', teacher: "Nguyen Van A", year: 2020-2021 },
-  { date: "22/12/2022", sb_name: "Math", block: '5', teacher: "Nguyen Van A", year: 2021-2022 },
-  { date: "12/12/2021", sb_name: "Math", block: '2', teacher: "Nguyen Van B", year: 2019-2020 },
-  { date: "02/12/2020", sb_name: "Math", block: '3', teacher: "Nguyen Van C", year: 2020-2021 },
-  { date: "22/12/2021", sb_name: "Math", block: '4', teacher: "Nguyen Van E", year: 2021-2022 },
-  { date: "12/12/2020", sb_name: "Math", block: '5', teacher: "Nguyen Van F", year: 2020-2021 },
-  { date: "22/12/2022", sb_name: "Math", block: '5', teacher: "Nguyen Van G", year: 2021-2022 },
+const students = [
 
+
+  {
+    username: "vuchau.190200@LA0102",
+    role: 3,
+    name: "Chau Hoai Vu",
+    address: "DH - LA",
+    phoneNumber: "12345678" ,
+    email: "vuchau@gmail.com",
+    dob: null,
+    schoolName: null,
+    className: "12A2",
+    headTeacherName: "Cao Tien Dung",
+  },
+ 
 
 ]
 
-function App() {
-  const [filteredData,setFilteredData]=useState(empList)
+function ALL_Students() {
+  const [filteredData,setFilteredData]=useState(students)
  const [filter, setFilter]=useState(true)
  const [year,setYear]=useState('all')
   const columns = [
-    { title: "Date", field: "date" },
-    { title: "Subject Name", field: "sb_name" },
-    { title: "Block", field: "block" },
-    { title: "Teacher", field: 'teacher' },
+   
+    { title: "username", field: "username" },
+    { title: "role", field: "role" },
+    { title: "name", field: "name" },
+    { title: "address", field: "address" }, 
+    { title: "phoneNumber", field: "phoneNumber" },
+    { title: "email", field: "email" },
+    { title: "dob", field: "dob" },
+    { title: "schoolName", field: "schoolName" }, 
+    { title: "className", field: "className" },
+    { title: "headTeacherName", field: "headTeacherName" }, 
+    
     
   ]
   const handleChange=()=>{
    setFilter(!filter)
   }
   useEffect(()=>{
-setFilteredData(year==='all'?empList:empList.filter(dt=>dt.year===year))
+setFilteredData(year==='all'?students:students.filter(dt=>dt.year===year))
 
   },[year])
 
   return (
     <div className="App">
-      <h1 align="center"></h1>
-      <h4 align='center'>Student Time Table</h4>
+      
+      <h4 align='center'>Student Information</h4>
       
       
       <MaterialTable
@@ -70,15 +82,18 @@ setFilteredData(year==='all'?empList:empList.filter(dt=>dt.year===year))
             onChange={(e)=>setYear(e.target.value)}
           >
              <MenuItem value={"all"}><em>All</em></MenuItem>
-            <MenuItem value={2019-2020}>2019-2020</MenuItem>
-            <MenuItem value={2020-2021}>2020-2021</MenuItem>
-            <MenuItem value={2021-2022}>2021-2022</MenuItem>
+            <MenuItem value={"2019-2020"}>2019-2020</MenuItem>
+            <MenuItem value={"2020-2021"}>2020-2021</MenuItem>
+            <MenuItem value={"2021-2022"}>2021-2022</MenuItem>
           </Select>,
           tooltip:"Filter Year",
           isFreeAction:true
 
           
           },
+
+
+  
 
 
   
@@ -90,4 +105,4 @@ setFilteredData(year==='all'?empList:empList.filter(dt=>dt.year===year))
   );
 }
 
-export default App;
+export default ALL_Students;
